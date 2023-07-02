2023ac8c47230cd06fc9bedab6610e257d3b1031
2795215389 <2795215389@qq.com>
2023-07-02 17:08:30 +0800

    分布式事务Seata

---
+++ account-service/src/main/java/cn/itcast/account/service/AccountTCCService.java
@@ -0,0 +1,19 @@
+package cn.itcast.account.service;
+
+import io.seata.rm.tcc.api.BusinessActionContext;
+import io.seata.rm.tcc.api.BusinessActionContextParameter;
+import io.seata.rm.tcc.api.LocalTCC;
+import io.seata.rm.tcc.api.TwoPhaseBusinessAction;
+
+@LocalTCC
+public interface AccountTCCService {
+    //@TwoPhaseBusinessAction注解标记了try
+    //@BusinessActionContextParameter将参数放入BusinessActionContext，用于后序使用
+    @TwoPhaseBusinessAction(name = "deduct", commitMethod = "confirm", rollbackMethod = "cancel")
+    void deduct(@BusinessActionContextParameter(paramName = "userId") String userId,
+                @BusinessActionContextParameter(paramName = "money")int money);
+    //confirm
+    boolean confirm(BusinessActionContext ctx);
+    //rollback
+    boolean cancel(BusinessActionContext ctx);
+}
---
+++ account-service/src/main/java/cn/itcast/account/service/impl/AccountTCCServiceImpl.java
@@ -0,0 +1,80 @@
+package cn.itcast.account.service.impl;
+
+import cn.itcast.account.entity.AccountFreeze;
+import cn.itcast.account.mapper.AccountFreezeMapper;
+import cn.itcast.account.mapper.AccountMapper;
+import cn.itcast.account.service.AccountTCCService;
+import io.seata.core.context.RootContext;
+import io.seata.rm.tcc.api.BusinessActionContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+public class AccountTCCServiceImpl implements AccountTCCService {
+
+    @Autowired
+    private AccountMapper accountMapper;
+    @Autowired
+    private AccountFreezeMapper freezeMapper;
+
+    @Override
+    @Transactional
+    public void deduct(String userId, int money) {
+        // 0.获取事务id
+        String xid = RootContext.getXID();
+        //业务悬挂判断
+        AccountFreeze flag = freezeMapper.selectById(xid);
+        if(flag != null){
+            //说明之前执行了cancel
+            return;
+        }
+        // 1.扣减可用余额
+        accountMapper.deduct(userId, money);
+        // 2.记录冻结金额，事务状态
+        AccountFreeze freeze = new AccountFreeze();
+        freeze.setUserId(userId);
+        freeze.setFreezeMoney(money);
+        freeze.setState(AccountFreeze.State.TRY);
+        freeze.setXid(xid);
+        freezeMapper.insert(freeze);
+    }
+
+    @Override
+    public boolean confirm(BusinessActionContext ctx) {
+        // 1.获取事务id
+        String xid = ctx.getXid();
+        // 2.根据id删除冻结记录
+        int count = freezeMapper.deleteById(xid);
+        return count == 1;
+    }
+
+    @Override
+    public boolean cancel(BusinessActionContext ctx) {
+        // 0.查询冻结记录
+        String xid = ctx.getXid();
+        AccountFreeze freeze = freezeMapper.selectById(xid);
+        String userId = ctx.getActionContext("userId").toString();
+        //空回滚判断，为空说明没有try
+        if(freeze == null){
+            freeze.setUserId(userId);
+            freeze.setFreezeMoney(0);
+            freeze.setState(AccountFreeze.State.CANCEL);
+            freeze.setXid(xid);
+            freezeMapper.insert(freeze);
+        }
+        //幂等校验
+        if(freeze.getState() == AccountFreeze.State.CANCEL){
+            return true;
+        }
+        // 1.恢复可用余额
+        accountMapper.refund(freeze.getUserId(), freeze.getFreezeMoney());
+        // 2.将冻结金额清零，状态改为CANCEL
+        freeze.setFreezeMoney(0);
+        freeze.setState(AccountFreeze.State.CANCEL);
+        int count = freezeMapper.updateById(freeze);
+        return count == 1;
+    }
+}
